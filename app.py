3228215ade7fc9646d0590eae88a1ad4ea7e214b
meghanvl <meghanvluoma@gmail.com>
2021-01-15 08:04:31 -0600

    updates

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
-from flask import Flask
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return f"Available Routes:"
-
-@app.route("/api/v1.0/precipitation")
-
-
-
-
-@app.route("/api/v1.0/stations")
-
-
-
-
-@app.route("/api/v1.0/tobs")
-
-
-
-
-@app.route("/api/v1.0/<start>")
-
-
-
-@app.route("/api/v1.0/<start>/<end>")
+            "/api/v1.0/precipitation"
+            "/api/v1.0/stations"
+            "/api/v1.0/tobs"
+            "/api/v1.0/<start>"
+            "/api/v1.0/<start>/<end>"
