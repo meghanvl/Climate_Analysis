8337efe86af072b9e414764b90763d538a68fe06
meghanvl <meghanvluoma@gmail.com>
2021-01-15 17:23:01 -0600

    updated histogram

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-from flask import Flask
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 app = Flask(__name__)
 
@@ -10,3 +25,7 @@ def home():
             "/api/v1.0/tobs"
             "/api/v1.0/<start>"
             "/api/v1.0/<start>/<end>"
+            
+
+if __name__ == "__main__":
+    app.run(debug=True)
